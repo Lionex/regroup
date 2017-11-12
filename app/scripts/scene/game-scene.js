ce9296f944b3f371943099ba1c7bd0bff70ec496
Gwen Lofman <Gwen@Lofman.co>
2017-11-11 15:55:18 -0900

    Scale objects on grab and release
    Abstract this behaviour using a function that will modify the
    behaviour of an object to scale by a certain factor when it's
    grabbed or released

--- app/scripts/scene/game-scene.js
+++ app/scripts/scene/game-scene.js
@@ -6,20 +6,26 @@ PixiGame.GameScene = function() {
     this.setup();
 };
 
+
 PixiGame.GameScene.constructor = PixiGame.GameScene;
+
+
 PixiGame.GameScene.prototype = Object.create(PIXI.Graphics.prototype);
+
 
 PixiGame.GameScene.prototype.setup = function() {
     this._objects = new PIXI.Container()
 
-    this._objects.addChild(
-        new_object(window.innerWidth / 2, 320)
-    )
+    var ob = new_object(window.innerWidth / 2, 320)
+    scale_on_grab(ob, 1.1)
+    this._objects.addChild(ob)
 
     this.addChild(this._objects)
 }
 
+
 PixiGame.GameScene.prototype.update = function() {}
+
 
 PixiGame.GameScene.prototype.destroy = function() {
     this.removeChildren();
@@ -77,4 +83,29 @@ function new_object(x,y) {
     }
 
     return ob
+}
+
+
+function scale_on_grab(ob, factor) {
+    ob
+        // Grab the object
+        .on('mousedown', grab)
+        .on('touchstart', grab)
+        // release the object
+        .on('mouseup', release)
+        .on('touchend', release)
+        .on('mouseupoutside', release)
+        .on('touchupoutside', release)
+
+    // Save reference which allows us to grab the pointer position
+    function grab(event) {
+        this.scale.x *= factor
+        this.scale.y *= factor
+    }
+
+    // Remove reference and stop the drag behaviour
+    function release() {
+        this.scale.x /= factor
+        this.scale.y /= factor
+    }
 }
